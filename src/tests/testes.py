470737a84880c56b723cc6d7228d39814bf10740
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-04-15 21:35:31 -0300

    3º Passo
    Teste para o número "V"

--- src/tests/testes.py
+++ src/tests/testes.py
@@ -5,8 +5,13 @@ __author__ = 'phillipe'
 
 
 class ConversorNumeroRomanoTest(unittest.TestCase):
+    def setUp(self):
+        self.conversor = ConversorDeNumeroRomano()
 
     def testDeveEntenderOSimboloI(self):
-        conversor = ConversorDeNumeroRomano()
-        numero = conversor.converte("I")
+        numero = self.conversor.converte("I")
         self.assertEqual(1, numero)
+
+    def testDeveEntenderOSimboloV(self):
+        numero = self.conversor.converte("V")
+        self.assertEqual(5, numero)
