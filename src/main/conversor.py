68370144abbd40de1c3486e7bc9f8de68f920c10
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-04-15 21:40:43 -0300

    5º Passo
    Números de uma letra funcionando

--- src/main/conversor.py
+++ src/main/conversor.py
@@ -3,8 +3,15 @@ __author__ = 'phillipe'
 
 class ConversorDeNumeroRomano:
 
+    tabela = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
     def converte(self, numero):
-        if numero == "I":
-            return 1
-        if numero == "V":
-            return 5
+        return self.tabela.get(numero)
