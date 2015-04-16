8c28d373c0b668cfe574af6ba0c5154afae7314f
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2015-04-15 21:55:16 -0300

    10º Passo
    Teste para entender números invertidos passando

--- src/main/conversor.py
+++ src/main/conversor.py
@@ -2,7 +2,6 @@ __author__ = 'phillipe'
 
 
 class ConversorDeNumeroRomano:
-
     tabela = {
         'I': 1,
         'V': 5,
@@ -15,8 +14,21 @@ class ConversorDeNumeroRomano:
 
     def converte(self, numero):
         acumulador = 0
+        ultimo_vizinho_da_direita = 0
 
-        for letra in numero:
-            acumulador += self.tabela[letra]
+        for letra in reversed(numero):
+            # pega o inteiro referente ao simbolo atual
+            atual = self.tabela[letra]
+
+            # se o da direita for menor, o multiplicaremos
+            # por -1 para torná-lo negativo
+            multiplicador = 1
+            if atual < ultimo_vizinho_da_direita:
+                multiplicador = -1
+
+            acumulador += atual * multiplicador
+
+            # atualiza o vizinho da direita
+            ultimo_vizinho_da_direita = atual
 
         return acumulador
